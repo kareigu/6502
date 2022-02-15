bd2e14de55fd4a161951bec306d173bb013b167e
mxr <eetu.kivelae@gmail.com>
2022-02-15 11:38:18 +0200

    Switched to using FTXUI for info display

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,5 +1,8 @@
 #include "cpu.hpp"
 #include "fmt/core.h"
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/screen/string.hpp"
 
 CPU::CPU(Memory* memory) 
   : mem(memory) 
@@ -8,34 +11,110 @@ CPU::CPU(Memory* memory)
   fmt::print("CPU Initialised\n");
 }
 
-
-static void print_register(const char* label, Byte byte) {
-  fmt::print("{}: {:#x}\n", label, byte);
-}
-
-static void print_register(const char* label, Bit bit) {
-  fmt::print("{}: {:b}\n", label, bit);
-}
-
-static void print_register(const char* label, WORD word) {
-  fmt::print("{}: {:#x}\n", label, word);
-}
-
 void CPU::print_registers() {
-  print_register("Accumulator", A);
-  print_register("X", X);
-  print_register("Y", Y);
+  auto main_registers = [&] {
+    auto content = ftxui::vbox({
+      ftxui::hbox({
+        ftxui::text(L"ACC: "),
+        ftxui::filler(),
+        ftxui::text(fmt::format("{:#x}", A)) | ftxui::bold
+      }) | ftxui::color(ftxui::Color::Green),
+      ftxui::hbox({
+        ftxui::text(L"X: "), 
+        ftxui::filler(),
+        ftxui::text(fmt::format("{:#x}", X)) | ftxui::bold
+      }) | ftxui::color(ftxui::Color::Blue),
+      ftxui::hbox({
+        ftxui::text(L"Y: "), 
+        ftxui::filler(),
+        ftxui::text(fmt::format("{:#x}", Y)) | ftxui::bold
+      }) | ftxui::color(ftxui::Color::Yellow),
+    });
 
-  fmt::print("Status Flags:\n");
-  print_register("N", N);
-  print_register("V", V);
-  print_register("-", false);
-  print_register("B", B);
-  print_register("D", D);
-  print_register("I", I);
-  print_register("Z", Z);
-  print_register("C", C);
+    return ftxui::window(ftxui::text("Main Registers"), content);
+  };
 
-  print_register("Stack Pointer", S);
-  print_register("Program Counter", PC);
+  auto status_flags = [&] {
+    auto content = ftxui::hbox({
+      ftxui::vbox({
+        ftxui::hbox({
+          ftxui::text(L"N: "), 
+          ftxui::text(fmt::format("{:b}", N)) | ftxui::bold
+        }),
+        ftxui::hbox({
+          ftxui::text(L"V: "), 
+          ftxui::text(fmt::format("{:b}", V)) | ftxui::bold
+        }),
+        ftxui::hbox({
+          ftxui::text(L"-: "), 
+          ftxui::text(fmt::format("{:b}", false)) | ftxui::bold
+        }),
+      })  | ftxui::color(ftxui::Color::Cyan),
+      ftxui::separator(),
+      ftxui::vbox({
+        ftxui::hbox({
+          ftxui::text(L"B: "), 
+          ftxui::text(fmt::format("{:b}", B)) | ftxui::bold
+        }),
+        ftxui::hbox({
+          ftxui::text(L"D: "), 
+          ftxui::text(fmt::format("{:b}", D)) | ftxui::bold
+        }),
+        ftxui::hbox({
+          ftxui::text(L"I: "), 
+          ftxui::text(fmt::format("{:b}", I)) | ftxui::bold
+        }),
+      }) | ftxui::color(ftxui::Color::Cyan),
+      ftxui::separator(),
+      ftxui::vbox({
+        ftxui::hbox({
+          ftxui::text(L"Z: "), 
+          ftxui::text(fmt::format("{:b}", Z)) | ftxui::bold
+        }),
+        ftxui::hbox({
+          ftxui::text(L"C: "), 
+          ftxui::text(fmt::format("{:b}", C)) | ftxui::bold
+        }),
+      }) | ftxui::color(ftxui::Color::Cyan)
+      | ftxui::flex
+    });
+
+    return ftxui::window(ftxui::text("Status Flags"), content);
+  };
+
+  auto other_info = [&] {
+    auto content = ftxui::vbox({
+      ftxui::hbox({
+        ftxui::text(L"SP: "),
+        ftxui::filler(),
+        ftxui::text(fmt::format("{:#x}", S))
+      }) | ftxui::color(ftxui::Color::Gold1),
+      ftxui::hbox({
+        ftxui::text(L"PC: "),
+        ftxui::filler(),
+        ftxui::text(fmt::format("{:#x}", PC))
+      }) | ftxui::color(ftxui::Color::Red),
+      ftxui::hbox({
+        ftxui::text(L"Memory: "),
+        ftxui::filler(),
+        ftxui::text(fmt::format("{:d} bytes", RAM_SIZE))
+      }) | ftxui::color(ftxui::Color::Default),
+    });
+
+    return ftxui::window(ftxui::text("Other Info"), content);
+  };
+
+  auto view = ftxui::vbox({
+    ftxui::hbox({
+      main_registers(),
+      status_flags(),
+      other_info()
+    })
+  });
+
+  auto screen = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(view));
+  ftxui::Render(screen, view);
+
+  fmt::print("{}\n", screen.ToString());
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
 #include "memory.hpp"
 #include "cpu.hpp"
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/screen/string.hpp"
 
 int main() {
   Memory ram;
   CPU cpu = CPU(&ram);
 
   cpu.print_registers();
-  return 0;
+
+  return EXIT_SUCCESS;
 }
