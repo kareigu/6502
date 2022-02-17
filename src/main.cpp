b2c120abf2ef98f9155852c2a209761ce969b5fb
mxr <eetu.kivelae@gmail.com>
2022-02-17 11:57:24 +0200

    Removed unnecessary includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <string>
-#include "memory.hpp"
-#include "cpu.hpp"
 #include "pc.hpp"
-#include "ftxui/dom/elements.hpp"
-#include "ftxui/screen/screen.hpp"
-#include "ftxui/screen/string.hpp"
-#include "ftxui/component/screen_interactive.hpp"
-#include "ftxui/component/component.hpp"
-#include "fmt/core.h"
-#include "asmb.hpp"
-
 
 
 int main() {
