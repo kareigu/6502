19447749d9e0e4cbe2d3887b45b644b5c623e341
mxr <eetu.kivelae@gmail.com>
2022-02-17 15:14:43 +0200

    Use shared pointer for memory

--- src/memory.hpp
+++ src/memory.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 
 
 #define Byte uint8_t
@@ -17,7 +18,7 @@
 #define SCREEN_RAM_END SCREEN_RAM_START + (SCREEN_WIDTH * SCREEN_HEIGHT) * BYTES_PER_CHARACTER
 
 struct Memory {
-  Byte* mem = new Byte[RAM_SIZE];
+  std::shared_ptr<Byte[]> mem = std::make_shared<Byte[RAM_SIZE]>();
 
   void init_ram();
   void fill_screen_ram(char c);
